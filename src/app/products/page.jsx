18d801ed3fc3c18b3b1926693776c9e43e6f2598
jk-jumur <developer.jumur@gmail.com>
2026-07-05 03:31:25 +0600

    understanding force cache and  cache checking

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,22 +1,29 @@
 import Product from "@/components/product";
 
-const getPosts = async()=>{
-     const res = await fetch('http://127.0.0.1:5000/products');
-     return res.json();
+
+const getProducts = async () => {
+      const res = await fetch("http://localhost:5000/products",
+          {
+            cache: "force-cache" })
+      return res.json();
 }
 
-const Productspage = async () => {
-      const products = await getPosts();
+const Productspage =  async() => {
+     const products = await getProducts();
+     console.log(products);
+      
     return (
         <div>
-             <h2>Products: {products.length}</h2>
+           <h2>Products: {products.length}</h2>
 
-             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                  {
-                    products.map(product => <Product key={product.id}
-                    product={product}></Product>)
-                  }
-             </div>
+           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+                 {
+                     products.map(product => <Product key={product.id} product={product}/>)
+
+                 };
+
+           </div>
         </div>
     );
 };
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -8,7 +8,7 @@ const Product = ({product}) => {
   <div className="card-body">
     <h2 className="card-title">{name}</h2>
     <p>{description}</p>
-    <p>${price}</p>
+    <p>$${price}</p>
     <div className="justify-end card-actions">
       <button className="btn btn-primary">Buy Now</button>
     </div>
